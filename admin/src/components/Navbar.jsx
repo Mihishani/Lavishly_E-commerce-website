0beae53264ba3400dc30c0e485d189375d41ef19
Your Name <you@example.com>
2024-06-21 13:18:34 +0530

    updated admin navigation bar and side bar

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+/*
 import logo from "../assets/images/the (1).png";
 
 const Navbar = () => {
@@ -8,10 +9,10 @@ const Navbar = () => {
       }
     >
       <div>
-        <img src={logo} alt={""} />
+        <img src={logo} alt={""} height={150} width={150} />
         <div
           className={
-            "uppercase bold-22 text-secondary px-3 rounded-md tracking-widest line-clamp-1 max-xs:bold-18 max-xs:py-2 max_xs:px-1"
+            "uppercase bold-22 text-secondary px-3 rounded-md tracking-widest line-clamp-1 max-xs:bold-18 max-xs:py-2 max_xs:px-1 "
           }
         >
           ADMIN PANEL
@@ -20,4 +21,31 @@ const Navbar = () => {
     </nav>
   );
 };
+export default Navbar;
+*/
+import logo from "../assets/images/the (1).png";
+
+const Navbar = () => {
+  return (
+    <nav
+      className={
+        "max_padd_container flex bg-white py-2 ring-1 ring-slate-900/5 relative"
+      }
+    >
+      <div className="flex items-center">
+        <img src={logo} alt={""} height={150} width={150} />
+      </div>
+      <div className="flex flex-1 items-center justify-center">
+        <div
+          className={
+            "uppercase bold-22  text-secondary px-3 rounded-md tracking-widest line-clamp-1 max-xs:bold-18 max-xs:py-2 max_xs:px-1 text-center"
+          }
+        >
+          ADMIN PANEL
+        </div>
+      </div>
+    </nav>
+  );
+};
+
 export default Navbar;
--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -1,4 +1,35 @@
+import { Link } from "react-router-dom";
+import addProduct from "../assets/images/AddProduct-removebg-preview.png";
+import listProduct from "../assets/images/clipboard-icon-front-side-white-background-removebg-preview.png";
+
 const Sidebar = () => {
-  return <div>sidebar</div>;
+  return (
+    <div
+      className={
+        "py-7 flex justify-center gap-x-1 gap-y-5 w-full bg-white sm:gap-x-4 lg:flex-col lg:pt-20 lg:max-w-60 lg:h-screen lg:justify-start lg:pl-6"
+      }
+    >
+      <Link to={"/addproduct"}>
+        <button
+          className={
+            "flexCenter gap-2 rounded-md bg-primary h-14 w-44 medium-16"
+          }
+        >
+          <img src={addProduct} alt={""} height={55} width={55} />
+          <span>Add Product</span>
+        </button>
+      </Link>
+      <Link to={"/listproduct"}>
+        <button
+          className={
+            "flexCenter gap-2 rounded-md bg-primary h-14 w-44 medium-16"
+          }
+        >
+          <img src={listProduct} alt={""} height={55} width={55} />
+          <span>Add Product</span>
+        </button>
+      </Link>
+    </div>
+  );
 };
 export default Sidebar;
