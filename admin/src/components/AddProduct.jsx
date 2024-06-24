6511cc55b926ce42b9a0186da6d99cc9cb92920d
Your Name <you@example.com>
2024-06-24 20:23:56 +0530

    created product add form

--- admin/src/components/AddProduct.jsx
+++ admin/src/components/AddProduct.jsx
@@ -1,4 +1,74 @@
+import upload_area from "../assets/images/upload image.webp";
 const AddProduct = () => {
-  return <div>AddProduct</div>;
+  return (
+    <div className={"p-8 box-border bg-white w-full rounded-sm mt-4"}>
+      <div>
+        <h4 className={"bold-18 pb-2"}>Product Title : </h4>
+        <input
+          type={"text"}
+          name={"name"}
+          placeholder={"Type here..."}
+          className={
+            "bg-primary outline-none max-w-80 w-full py-3 px-4 rounded-md"
+          }
+        />
+      </div>
+      <div>
+        <h4 className={"bold-18 pb-2"}>Price : </h4>
+        <input
+          type={"text"}
+          name={"old_price"}
+          placeholder={"Type here..."}
+          className={
+            "bg-primary outline-none max-w-80 w-full py-3 px-4 rounded-md"
+          }
+        />
+      </div>
+      <div>
+        <h4 className={"bold-18 pb-2"}>Product Title : </h4>
+        <input
+          type={"text"}
+          name={"new_price"}
+          placeholder={"Type here..."}
+          className={
+            "bg-primary outline-none max-w-80 w-full py-3 px-4 rounded-md"
+          }
+        />
+      </div>
+      <div className={"mb-3 flex items-center gap-x-4"}>
+        <h4 className={"bold-18 pb-2"}>product category : </h4>
+        <select
+          name={"category"}
+          id={""}
+          className={
+            "bg-primary ring-1 ring-slate-900/20 medium-16 rounded-sm outline-none"
+          }
+        >
+          <option value={"skin"}>Skin</option>
+          <option value={"hair"}>Hair</option>
+          <option value={"makeup"}>Makeup</option>
+          <option value={"bags"}>Bags</option>
+          <option value={"earring"}>Earring</option>
+          <option value={"shoes"}>Shoes</option>
+        </select>
+      </div>
+      <div>
+        <label htmlFor={"file-input"}>
+          <img
+            src={upload_area}
+            alt={""}
+            className={"w-20 rounded-sm inline-block"}
+          />
+        </label>
+        <input
+          type={"file"}
+          name={"image"}
+          id={"file-input"}
+          hidden
+          className={"bg-primary max-w-80 w-full py-3 px-4"}
+        />
+      </div>
+    </div>
+  );
 };
 export default AddProduct;
--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -6,13 +6,13 @@ const Sidebar = () => {
   return (
     <div
       className={
-        "py-7 flex justify-center gap-x-1 gap-y-5 w-full bg-white sm:gap-x-4 lg:flex-col lg:pt-20 lg:max-w-60 lg:h-screen lg:justify-start lg:pl-6"
+        "py-7 flex justify-center gap-x-2 gap-y-5 w-full bg-white sm:gap-x-4 lg:flex-col lg:pt-20 lg:max-w-60 lg:h-screen lg:justify-start lg:pl-6"
       }
     >
       <Link to={"/addproduct"}>
         <button
           className={
-            "flexCenter gap-2 rounded-md bg-primary h-14 w-44 medium-16"
+            "flexCenter gap-2 rounded-md bg-primary h-14 w-40 xs:w-44 medium-14 sm:medium-16"
           }
         >
           <img src={addProduct} alt={""} height={55} width={55} />
@@ -22,7 +22,7 @@ const Sidebar = () => {
       <Link to={"/listproduct"}>
         <button
           className={
-            "flexCenter gap-2 rounded-md bg-primary h-14 w-44 medium-16"
+            "flexCenter gap-2 rounded-md bg-primary h-14 w-40 xs:w-44 medium-14 sm:medium-16"
           }
         >
           <img src={listProduct} alt={""} height={56} width={56} />
