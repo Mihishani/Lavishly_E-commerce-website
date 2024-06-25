fad529f5cc3dfe21c4612d710cf83fa3d7f3a49f
Your Name <you@example.com>
2024-06-25 21:51:32 +0530

    Created product list form and function

--- admin/src/components/ListProduct.jsx
+++ admin/src/components/ListProduct.jsx
@@ -1,4 +1,59 @@
+import { useEffect, useState } from "react";
+import { TbTrash } from "react-icons/tb";
+
 const ListProduct = () => {
-  return <div>ListProduct</div>;
+  const [allproducts, setAllproducts] = useState([]);
+
+  const fetchInfo = async () => {
+    await fetch("http://localhost:4000/allproducts")
+      .then((res) => res.json())
+      .then((data) => {
+        setAllproducts(data);
+      });
+  };
+
+  useEffect(() => {
+    fetchInfo();
+  }, []);
+
+  return (
+    <div>
+      <h4>Products List</h4>
+      <div>
+        <table className={"w-full mx-auto"}>
+          <thead>
+            <tr>
+              <th className={"p-2"}>Products</th>
+              <th className={"p-2"}>Title</th>
+              <th className={"p-2"}>Old Price</th>
+              <th className={"p-2"}>New Price</th>
+              <th className={"p-2"}>Category</th>
+              <th className={"p-2"}>Remove</th>
+            </tr>
+          </thead>
+          <tbody>
+            {allproducts.map((product, i) => (
+              <tr key={i}>
+                <td height={60} width={60}>
+                  <img src={product.image} alt={""} />
+                </td>
+                <td>
+                  <div>{product.name}</div>
+                </td>
+                <td className={"font-sans"}>RS. {product.old_price}</td>
+                <td className={"font-sans"}>RS. {product.new_price}</td>
+                <td>{product.category}</td>
+                <td>
+                  <div className={"text-red-700"}>
+                    <TbTrash />
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 export default ListProduct;
