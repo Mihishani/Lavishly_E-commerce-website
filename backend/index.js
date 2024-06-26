ea866d9f45c041bc8825b91b1bb2906627424762
Your Name <you@example.com>
2024-06-26 16:22:56 +0530

    Created user registration API

--- backend/index.js
+++ backend/index.js
@@ -336,6 +336,34 @@ const user = mongoose.model("User", {
 });
 
 //Creating endpoint for registering the user
+app.post("/signup", async (req, res) => {
+  let check = await User.findOne({ email: req.body.email });
+  if (check) {
+    return res.status(400).json({
+      success: false,
+      errors: "Existing user found with same email address",
+    });
+  }
+  let cart = {};
+  for (let i = 0; i < 300; i++) {
+    cart[i] = 0;
+  }
+  const user = new User({
+    name: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    cartData: cart,
+  });
+  await user.save();
+
+  const data = {
+    user: {
+      id: user.id,
+    },
+  };
+  const token = jwt.sign(data, "secret_ecom");
+  res.json({ success: true, token });
+});
 
 app.listen(port, (error) => {
   if (!error) {
